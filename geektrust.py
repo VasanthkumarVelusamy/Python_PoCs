16f7392204e85f9fb04ba14fa0baa0801869ac61
Vasanth Kumar <vasanthkumar.v@hotmail.com>
2017-11-10 18:46:22 +0530

    minor changes in the assignment program

--- geektrust.py
+++ geektrust.py
@@ -1,56 +1,57 @@
+# Tame of Thrones!
 emblem = {'Land':'PANDA','Water':'OCTOPUS','Ice':'MAMMOTH','Air':'OWL','Fire':'DRAGON'}
 allies = []
 ruler = None
 
-def invite(kingdom):
-    global message, allies
+# Function to invite kingdom with message.
+def invite(kingdom, message):
+    global allies
 
     for c in emblem[kingdom]:            # Gets the animal for the given kingdom and iterate through the charaters.
-        found = False
+        if c in message:               # Checks if the character is present in the message.
+            message = message.replace(c,'',1)         # removes the first occurence of the charater matched.
+        else:
+            return
+    allies.append(kingdom)          # Allies that have accepted the invitation are appended to the list.
 
-        if i in message:
-            message = message.replace(i,'',1)         # removes the first occurence of the charater matched with animal from the message.
-            found = True
-        if not found:
-            return False
-
-    allies.append(kingdom)
-    return True
-
+# Function to print Ruler and Allies information.
 def allies_info():
+
     global allies, ruler
 
-    if len(allies) == 0:
-        _allies = None
-    else:
-        _allies = {x:y for (y, x) in enumerate(allies)}.keys()         # Adding the allies as keys in dictionary to avoid duplicates and also to keep insertion order.
-        _allies = ', '.join(map(str,_allies))
-    if len(_allies) >= 3:
+    unique_allies = {x:y for (y, x) in enumerate(allies)}.keys()      # Adding the allies as keys in dictionary to avoid duplicates and also to keep insertion order.
+    allies_string = ', '.join(map(str,unique_allies))        # String to print allies.
+
+    if len(unique_allies) == 0:
+        allies_string = None
+    elif len(unique_allies) >= 3:
         ruler = 'King Shan'
 
-    print('Who is the ruler of Southeos?')
+    print('Who is the ruler of Southeros?')
     print(ruler)
     print('Allies of King Shan?')
-    print(_allies)
+    print(allies_string)
+
+# Function to get message and invite kingdom.
+def get_message_and_invite():
+    while True:           # Reading input lines until getting an empty line.
+        line = input()
+
+        if line:
+            try:
+                kingdom, message = line.split(',')
+                message = message.strip(' \"').upper()
+                kingdom = kingdom.capitalize()
+                invite(kingdom,message)        # Invite kingdom with message.
+            except ValueError:
+                print('Error: Invalid input!')
+            except KeyError:
+                print('Error: Invalid kingdom!')
+        else:
+            break
 
 allies_info()
 print('')
 print('Input Messages to kingdoms from King Shan:')
-
-while True:           # Reading input lines until getting an empty line.
-    line = input()
-
-    if line:
-        try:
-            kingdom, message = line.split(',')
-            message = message.strip(' \"').upper()
-            kingdom = kingdom.capitalize()
-            invite(kingdom)
-        except ValueError:
-            print('Error: Invalid input!')
-        except KeyError:
-            print('Error: Invalid kingdom!')
-    else:
-        break
-
+get_message_and_invite()
 allies_info()
